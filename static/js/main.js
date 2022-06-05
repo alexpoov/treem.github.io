95b69cbab01fff4fafb42f2f0db1ae14178c3871
Aleksandr Popov <56491959+alexpoov@users.noreply.github.com>
2022-06-05 18:06:48 +0300

    initial commit

---
+++ static/js/main.js
@@ -0,0 +1,59 @@
+// confirm password
+const reg_form = document.getElementById("register_form");
+
+function validatePassword(){
+  var password = document.getElementById('newtrpassword')
+  var confirm_password = document.getElementById('newtrpassword_repeat')
+    if (password.value=="")
+    {
+        password.setCustomValidity("Passwords must not be empty");
+        return false;
+      } 
+    else if (password.value != confirm_password.value) 
+    {
+        confirm_password.setCustomValidity("Пароли не совпадают");
+        return false;
+    } else {
+        confirm_password.setCustomValidity('');
+        return true;
+    }
+}
+
+reg_form.elements['newtrpassword'].onchange = validatePassword;
+reg_form.elements['newtrpassword_repeat'].onkeyup = validatePassword;
+
+// post to /register request
+
+reg_form.addEventListener('submit', async (e) => {
+  if (validatePassword() === true){
+    e.preventDefault();
+    const data = new FormData(reg_form);
+    const value = Object.fromEntries(data);
+    const options = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(value), 
+      redirect: 'http://localhost:5000/reg_thanks.html/'
+    };
+    fetch('http://localhost:8000/register/', 
+      options)
+
+    // console.log(options) // DELETE AFTER DEBUG!!!
+
+}})
+
+// post to /api-token-auth request
+
+// log_form.addEventListener('submit', async (e) => {
+//   e.preventDefault();
+//   const data = new FormData(log_form);
+//   const value = Object.fromEntries(data.entries());
+//   const options = {
+//     method: 'POST',
+//     headers: {'Content-Type': 'application/json'},
+//     body: JSON.stringify(value)
+//   };
+//   fetch('http://localhost:8000/register/', options)
+// })
+
+
